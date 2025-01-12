e791028b38efd48a4592fced6e7f6a05af756385
Sind96 <sindhujany@gmail.com>
2025-01-12 23:13:16 +0000

    Hello World working

--- server/index.js
+++ server/index.js
@@ -3,12 +3,11 @@ const app = express();
 var cors = require("cors");
 
 const port = 3000;
+const router = require("./router");
 
 app.use(cors());
 app.use(express.json());
 app.use(router);
-
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
