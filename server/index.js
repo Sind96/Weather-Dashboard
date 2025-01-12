b97592d2d94ea924200fb3adcc8bfaaecce7ba6f
Sind96 <sindhujany@gmail.com>
2025-01-12 23:10:50 +0000

    Express "Hello World"

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
-import express from "express";
+const express = require("express");
 const app = express();
+var cors = require("cors");
+
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
