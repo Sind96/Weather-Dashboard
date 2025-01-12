c45e06850fb0ce193e89c7884215d4d36e6d774a
Sind96 <sindhujany@gmail.com>
2025-01-12 22:55:17 +0000

    Tailwind added.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,9 @@ import "./index.css";
 function App() {
   return (
     <>
-      <p>Hello</p>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Hello world!
+      </h1>
     </>
   );
 }
