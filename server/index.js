611b6605a5feb0c411b5004ccb0996622e46ef7b
Sind96 <sindhujany@gmail.com>
2025-01-15 22:51:40 +0000

    index.js done

--- server/index.js
+++ server/index.js
@@ -1,14 +1,25 @@
 const express = require("express");
-const app = express();
-var cors = require("cors");
-
-const port = 3000;
+const cors = require("cors");
+const mongoose = require("mongoose");
 const router = require("./router");
 
+const app = express();
+const port = 3000;
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(router);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+// Async function to connect to MongoDB & Start Server
+(async function () {
+  try {
+    mongoose.connect(process.env.MONGO_URI);
+    app.listen(port);
+    console.log(
+      `Server running on PORT ${port} and Database has successfully connected!🕊️`
+    );
+  } catch (error) {
+    console.log(`Server & Database could not connect:`, error);
+  }
+})();
