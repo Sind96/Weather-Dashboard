6ddf136b65c1b9fc824d6a4d5a157adea8d34fa0
Sind96 <sindhujany@gmail.com>
2025-01-16 22:44:38 +0000

    Adding Homepage / adding routing to App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 import "./index.css";
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline text-red-500">
-        Hello world!
-      </h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
