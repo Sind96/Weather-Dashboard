299bbf5a9df5227b58ad1ca76eb9628ef41e0d81
Sind96 <sindhujany@gmail.com>
2025-01-16 22:54:36 +0000

    Creating SearchBar

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import SearchBar from "../components/SearchBar";
 
 export default function Home() {
   return (
     <div>
-      <h1 className="text-2xl text-red-700">Test</h1>
+      <SearchBar />
     </div>
   );
 }
