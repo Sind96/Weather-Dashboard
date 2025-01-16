76d91332533f03b960d4302613979cd45ead3a5e
Sind96 <sindhujany@gmail.com>
2025-01-16 23:00:47 +0000

    search bar - form input done

--- client/src/components/SearchBar.tsx
+++ client/src/components/SearchBar.tsx
@@ -1,10 +1,18 @@
 import { useState } from "react";
 
 export default function SearchBar() {
+  const [city, setCity] = useState("");
 
   return (
-    <div >
-      
+    <div>
+      <form>
+        <input
+          type="text"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          placeholder="Enter City"
+        ></input>
+      </form>
     </div>
   );
 }
