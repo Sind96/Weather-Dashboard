196b24e4727fc9e14376b4bfae4ebaa076e9c3b8
Sind96 <sindhujany@gmail.com>
2025-01-18 12:49:43 +0000

    minor changes again

--- client/src/components/SearchBar.tsx
+++ client/src/components/SearchBar.tsx
@@ -1,36 +1,29 @@
-import { useState } from "react";
-import { fetchWeather } from "../services/api.Services";
+export default function SearchBar({}) {
+  // const handleSearch = () => {
+  //     if (city) {
+  //       onSearch(city); // Call parent function to fetch data
+  //       setCity(""); // Clear input field
+  //     }
+  //   };
 
-export default function SearchBar() {
-  const [city, setCity] = useState("");
-
-  const handleSearch = async (city: string, event) => {
-    event.preventDefault();
-    try {
-      const data = await fetchWeather(city);
-      console.log(data);
-    } catch (error) {
-      console.error(error.message);
-    }
+    return (
+      <div className="flex items-center gap-2">
+        <form>
+          <input
+            type="text"
+            value={city}
+            // onChange={(e) => setCity(e.target.value)}
+            placeholder="Enter City"
+            className="border px-2 py-1"
+          ></input>
+          <button
+            // onClick={handleSearch}
+            className="bg-blue-500 text-white px-3 py-1"
+          >
+            Search
+          </button>
+        </form>
+      </div>
+    );
   };
-
-  return (
-    <div className="flex items-center gap-2">
-      <form>
-        <input
-          type="text"
-          value={city}
-          onChange={(e) => setCity(e.target.value)}
-          placeholder="Enter City"
-          className="border px-2 py-1"
-        ></input>
-        <button
-          onClick={handleSearch}
-          className="bg-blue-500 text-white px-3 py-1"
-        >
-          Search
-        </button>
-      </form>
-    </div>
-  );
 }
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,8 +1,20 @@
-import React from "react";
+import { useState } from "react";
 import SearchBar from "../components/SearchBar";
+import { fetchWeather } from "../services/api.Services";
 
 export default function Home() {
-  
+  const [weather, setWeather] = useState("");
+
+  const getWeather = async (city: string) => {
+    try {
+      const data = await fetchWeather(city);
+      console.log(data);
+      setWeather(data);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
   return (
     <div>
       <div className="container mx-auto">
