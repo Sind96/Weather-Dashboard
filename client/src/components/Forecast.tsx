91cd483bb4a3af976a7c9ba08962f94131dc03f9
Sind96 <sindhujany@gmail.com>
2025-01-20 13:54:21 +0000

    Register ChartJS

--- client/src/components/Forecast.tsx
+++ client/src/components/Forecast.tsx
@@ -11,6 +11,16 @@ import {
   Legend,
 } from "chart.js";
 
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
 export default function Forecast({ forecastWeatherData }: ForecastProps) {
   console.log(forecastWeatherData);
   return <div></div>;
