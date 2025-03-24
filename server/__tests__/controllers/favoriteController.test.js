46a3e5abb9884687f0b7c9683707088c8ff319e5
Sind96 <sindhujany@gmail.com>
2025-03-24 23:25:28 +0000

    favoriteController.test: Added test for getFavourites.

--- server/__tests__/controllers/favoriteController.test.js
+++ server/__tests__/controllers/favoriteController.test.js
@@ -8,3 +8,16 @@ import Favorite from "../../model/Favorite.js";
 
 beforeAll(async () => connectTestDB());
 afterAll(async () => disconnectTestDB());
+
+test("getFavourites should return an empty array initially", async () => {
+  const req = {};
+  const res = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  };
+
+  await getFavorites();
+
+  expect(res.status).toHaveBeenCalledWith(200);
+  expect(res.json).toHaveBeenCalledWith({ tasks: [] });
+});
