49a820394b5b25a4712fe08ce76a06817225325b
Sind96 <sindhujany@gmail.com>
2025-04-05 20:16:24 +0100

    favouriteRoute.test: POST test added.

--- server/__tests__/integration/favouriteRoute.test.js
+++ server/__tests__/integration/favouriteRoute.test.js
@@ -9,4 +9,14 @@ afterAll(async () => await disconnectTestDB());
 
 describe("Test API Endpoints", () => {
   beforeEach(async () => await Favorite.deleteMany({}));
+
+  test("POST /favourites should add favourite city to favourites", async () => {
+    const response = await request(app).post("/favorites").send({
+      city: "Integration Test City",
+    });
+
+    expect(response.status).toBe(201);
+    expect(response.body.newFavorite).toHaveProperty("_id");
+    expect(response.body.newFavorite.city).toBe("Integration Test City");
+  });
 });
